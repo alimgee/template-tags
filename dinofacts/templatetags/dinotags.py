1e26dd4fbb86d82ea92a36e7df79986d473c32d5
alanmcgee <mcgee.al@gmail.com>
2022-03-17 17:46:56 +0000

    Adding other letters tag

--- dinofacts/templatetags/dinotags.py
+++ dinofacts/templatetags/dinotags.py
@@ -9,3 +9,15 @@ def first_letters(iterable):
         result += item[0]
 
     return result
+
+
+@register.filter(name="nth_letters", is_safe=True)
+def other_letters(iterable, num):
+    result = ""
+    for item in iterable:
+        if len(item) <= num or not item[num - 1].isalpha():
+            result += " "
+        else:
+            result += item[num - 1]
+
+    return result
