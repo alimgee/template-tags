00507c6b18d227e8efc2c129cd468d4cefe1f211
alanmcgee <mcgee.al@gmail.com>
2022-03-18 15:49:54 +0000

    using custom tag with context

--- dinofacts/templatetags/dinotags.py
+++ dinofacts/templatetags/dinotags.py
@@ -65,3 +65,16 @@ def make_ul(iterable):
     content.append("</ul>")
     content = "".join(content)
     return mark_safe(content)
+
+
+@register.simple_tag(takes_context=True)
+def dino_list(context, title):
+    output = [f"<h2>{title}</h2><ul>"]
+    for dino in context["dinosaurs"]:
+        output.append(f"<li>{escape(dino)}</li>")
+
+    output.append("</ul>")
+    output = "".join(output)
+
+    context["weight"] = "20 tons"
+    return mark_safe(output)
