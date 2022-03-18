50515383ca7a38961e19fe0bbd0a96f32a3abf2f
alanmcgee <mcgee.al@gmail.com>
2022-03-18 16:04:26 +0000

    rendering content with advanced custom tags

--- dinofacts/templatetags/dinotags.py
+++ dinofacts/templatetags/dinotags.py
@@ -103,3 +103,32 @@ class MarkdownNode(template.Node):
         content = self.nodelist.render(context)
         result = mistune.markdown(str(content))
         return result
+
+
+@register.tag()
+def shownodes(parser, token):
+    nodelist = parser.parse(("endshownodes",))
+    parser.delete_first_token()
+    return ShowNodesNode(token, nodelist)
+
+class ShowNodesNode(template.Node):
+    def __init__(self, token, nodelist):
+        self.token = token
+        self.nodelist = nodelist
+
+    def render(self, context):
+        result = [
+            "<ul><li>Token info:</li><ul>",
+        ]
+
+        for part in self.token.split_contents():
+            content = escape(str(part))
+            result.append(f"<li>{content}</li>")
+
+        result.append("</ul><li>Block contents:</li><ul>")
+        for node in self.nodelist:
+            content = escape(str(node))
+            result.append(f"<li>{content}</li>")
+
+        result.append("</ul>")
+        return "".join(result)
